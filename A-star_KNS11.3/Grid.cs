25d21277acfb9efeaee8336be6b7588439c3d5e5
GodlikeAtheist <ya.yurchyshyn@nltu.lviv.ua>
2020-12-15 02:16:20 +0200

    Додав методи класу Grid для зміни кольори клітинок в dataGridView в залежності від роботи алгоритму

--- A-star_KNS11.3/Grid.cs
+++ A-star_KNS11.3/Grid.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace A_star_KNS11._3
 {
@@ -18,6 +20,36 @@ namespace A_star_KNS11._3
                 for(int x = 0; x < cells.GetLength(1); x++ )
                 {
                     cells[y, x] = new Cell();
+                }
+            }
+        }
+
+        public void PaintNeigbors(DataGridView grid)
+        {
+            for(int y = 0; y < cells.GetLength(0); y++)
+            {
+                for(int x = 0; x < cells.GetLength(1); x++)
+                {
+                    if(Neighbors[y, x] == true)
+                    {
+                        grid.CurrentCell = grid[y, x];
+                        grid.CurrentCell.Style.BackColor = Color.Blue;
+                    }
+                }
+            }
+        }
+
+        public void PaintChecked(DataGridView grid)
+        {
+            for (int y = 0; y < cells.GetLength(0); y++)
+            {
+                for (int x = 0; x < cells.GetLength(1); x++)
+                {
+                    if (Checked[y, x] == true)
+                    {
+                        grid.CurrentCell = grid[y, x];
+                        grid.CurrentCell.Style.BackColor = Color.Azure;
+                    }
                 }
             }
         }
